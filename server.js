354a4264df851faea22d47d09369e0a3f372a105
mariekelemmens <marieke.lemmens@student.kdg.be>
2018-08-23 18:45:27 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('/dist'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/starstruck/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 // Start the app by listening on the default Heroku port

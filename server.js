5d040fd33abd2ec1384eae677b465a3735c3d094
mariekelemmens <marieke.lemmens@student.kdg.be>
2018-08-23 18:16:37 +0200

    appname replacement

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ let path = require('path');
 let app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/starstruck@0.0.0'));
+app.use(express.static('/dist/starstruck'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname+'/dist/starstruck@0.0.0/index.html'));
+  res.sendFile(path.join('/dist/starstruck/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

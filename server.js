d0292d66c43f4ceab4a520df469b9afea2332b81
mariekelemmens <marieke.lemmens@student.kdg.be>
2018-08-23 18:47:39 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
+// server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('/dist'));
-
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
-
-// Start the app by listening on the default Heroku port
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);

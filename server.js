b7b4e0ce79c26e0f75637f94b3d6e33009aaa6db
mariekelemmens <marieke.lemmens@student.kdg.be>
2018-08-23 22:48:43 +0200

    test

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/*', function(req,res) {
 });
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/overwatch-heroes'));
+app.use(express.static(__dirname + '/dist/starstruck'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/overwatch-heroes/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/starstruck/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

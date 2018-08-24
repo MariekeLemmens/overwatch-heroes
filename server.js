8d8c6e62cfdb33c051077069dc84bc75c794fede
mariekelemmens <marieke.lemmens@student.kdg.be>
2018-08-24 19:51:39 +0200

    yeet

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+app.use(express.static(__dirname + '/dist'));
 
-// Serve only the static files form the dist directory
-app.use(express.static('/dist/starstruck'));
-
-app.get('/*', function(req,res) {
-  res.sendFile(path.join('/dist/starstruck/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
